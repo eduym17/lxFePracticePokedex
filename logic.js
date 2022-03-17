c04f08704f136b7d4564a732e56cc81ceac6f39b
eduym17 <eduardo_yanez@live.com.mx>
2022-03-17 12:18:19 -0600

    Read API info

--- logic.js
+++ logic.js
@@ -2,6 +2,7 @@ const fetchPokemon = () => {
   const pokeName = document.getElementById("pokeName");
   let pokeInput = pokeName.value.toLowerCase();
   const url = `https://pokeapi.co/api/v2/pokemon/${pokeInput}`;
+  const onClick = document.querySelector('.onClick');
 
   fetch(url).then((res) => {
     if(res.status != "200"){
@@ -10,12 +11,41 @@ const fetchPokemon = () => {
     return res.json();
 
   }).then((data) => {
+
+    const li = document.createElement('li');
+    li.classList.add('.pokeInfo');
+
+    const pokeImage = (url) => {
+      const pokeImg = document.getElementById("pokeImg");
+      pokeImg.src = url;
+    }
+
     let pokeImg = data.sprites.front_default;
     pokeImage(pokeImg);
-  })
-}
 
-const pokeImage = (url) => {
-  const pokeImg = document.getElementById("pokeImg");
-  pokeImg.src = url;
+    const pokeNameApi = document.createElement('span');
+    pokeNameApi.classList.add('pokeNameApi', 'pokeApi');
+    pokeNameApi.innerText = 'Nombre: ' + data.name;
+
+    const pokeTypeApi = document.createElement('span');
+    pokeTypeApi.classList.add('pokeTypeApi', 'pokeApi');
+    pokeTypeApi.innerText = 'Tipo: ' + data.types[0].type.name;
+
+    const pokeBasePowerApi = document.createElement('span');
+    pokeBasePowerApi.classList.add('pokeBasePowerApi', 'pokeApi');
+    pokeBasePowerApi.innerText = 'Poder base: ' + data.stats[0].base_stat + ', Esfuerzo base: ' + data.stats[0].effort;
+
+    const pokeMaxPowerApi = document.createElement('span');
+    pokeMaxPowerApi.classList.add('pokeMaxPowerApi', 'pokeApi');
+    pokeMaxPowerApi.innerText = 'Poder máximo: ' + data.stats[5].base_stat + ', Esfuerzo máximo: ' + data.stats[5].effort;
+
+    const pokeMove = document.createElement('span');
+    pokeMove.classList.add('pokeMove', 'pokeApi');
+    pokeMove.innerText = 'Movimiento: ' + data.moves[0].move.name;
+    
+    li.append(pokeNameApi, pokeTypeApi, pokeBasePowerApi, pokeMaxPowerApi, pokeMove);
+    document.querySelector('.data').appendChild(li);
+
+
+  });
 }
